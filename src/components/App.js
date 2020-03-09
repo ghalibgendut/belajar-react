bd3200bd36806e22c928315c4c9cfa1c76709478
ghalibgendut <ghalibgendut@gmail.com>
2020-03-09 09:59:56 +0700

    Belajar React

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+// ROOT COMPONENTS
+
+import React from 'react';
+
+// Membuat komponen berbasis class
+
+class App extends React.Component {
+
+    // functuin yang wajib ada, apa yang ingin kita tampilkan, akan kita 
+
+    render () {
+        return (
+            <div>
+                <h1> REACT 101 </h1>
+            </div>
+        )
+    }
+
+}
+
+// Karena akan di import di file lain
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+// React, digunakan untuk memproses kode menjadi sebuah komponen
 import React from 'react';
+
+// Digunakan untuk memanipulasi komponen atau menghubungkan app.js ke index.html
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// extension .js tidak wajib ditulis
+import App from './components/App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+// Menghubungkan App ke index.html di folder public
+ReactDOM.render(<App/>, document.getElementById('root'));
